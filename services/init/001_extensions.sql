c96f436b62348281744bd24ac321d63e74d602d1
Mikhail Kabakov <kabakovms@google.com>
2025-11-14 10:53:30 +0400

    fix init script and TZ

--- services/init/001_extensions.sql
+++ services/init/001_extensions.sql
@@ -1,5 +1,31 @@
-CREATE EXTENSION IF NOT EXISTS vector;
+-- Ensure the schema exists for pgvector objects
+CREATE SCHEMA IF NOT EXISTS registry;
+
+-- Create or move pgvector into the registry schema so dumps referencing registry.vector work
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'vector') THEN
+    EXECUTE 'ALTER EXTENSION vector SET SCHEMA registry';
+  ELSE
+    EXECUTE 'CREATE EXTENSION vector WITH SCHEMA registry';
+  END IF;
+END
+$$;
+
 CREATE EXTENSION IF NOT EXISTS pg_prewarm;
+
 \connect registry
-CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE SCHEMA IF NOT EXISTS registry;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'vector') THEN
+    EXECUTE 'ALTER EXTENSION vector SET SCHEMA registry';
+  ELSE
+    EXECUTE 'CREATE EXTENSION vector WITH SCHEMA registry';
+  END IF;
+END
+$$;
+
 CREATE EXTENSION IF NOT EXISTS pg_prewarm;
